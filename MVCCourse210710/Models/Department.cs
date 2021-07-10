66d5163f84bc98c790565ba284e1d45b1bcf77f4
Will <doggy.huang@gmail.com>
2021-07-10 15:29:30 +0800

    示範自訂錯誤驗證訊息的多國語系化設計

--- MVCCourse210710/Models/Department.cs
+++ MVCCourse210710/Models/Department.cs
@@ -9,6 +9,7 @@
 
 namespace MVCCourse210710.Models
 {
+    using MVCCourse210710.Resources;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
@@ -23,7 +24,8 @@ namespace MVCCourse210710.Models
     
         public int DepartmentID { get; set; }
 
-        [Required(ErrorMessage = "請輸入您的中文姓名({0})")]
+        [Required(ErrorMessageResourceType = typeof(Resource1),
+            ErrorMessageResourceName = "Department_Name_Required")]
         public string Name { get; set; }
         [Range(0, 99999, ErrorMessage = "請輸入合理的預算範圍 ({1} ~ {2})")]
         public decimal Budget { get; set; }
