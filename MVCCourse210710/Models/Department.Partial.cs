d644234c51ebda439815a2fe1c7301901b09951c
Will <doggy.huang@gmail.com>
2021-07-11 16:31:34 +0800

    改用繼承的手法，減少 MetadataType 中重複的驗證屬性

--- MVCCourse210710/Models/Department.Partial.cs
+++ MVCCourse210710/Models/Department.Partial.cs
@@ -9,25 +9,6 @@ namespace MVCCourse210710.Models
     public partial class Department
     {
     }
-    
-    public partial class DepartmentMetaData
-    {
-        [Required]
-        public int DepartmentID { get; set; }
-
-        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
-        [Required(ErrorMessageResourceType = typeof(Resource1), 
-            ErrorMessageResourceName = "Department_Name_Required")]
-        public string Name { get; set; }
-        [Required]
-        [Range(0, 100, ErrorMessage = "請輸入合理的預算 ({1} ~ {2})")]
-        public decimal Budget { get; set; }
-        [Required]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public System.DateTime StartDate { get; set; }
-        [UIHint("InstructorID")] 
-        public Nullable<int> InstructorID { get; set; }
-    }
 
     public partial class DepartmentCreateMetaData
     {
@@ -35,22 +16,18 @@ namespace MVCCourse210710.Models
         public string Name { get; set; }
         [Range(0, 100, ErrorMessage = "請輸入合理的預算範圍 ({1} ~ {2})")]
         public decimal Budget { get; set; }
-
         [UIHint("InstructorID")]
         public Nullable<int> InstructorID { get; set; }
     }
 
-    public partial class DepartmentEditMetaData
+    public partial class DepartmentEditMetaData : DepartmentCreateMetaData
     {
-        [Required(ErrorMessageResourceType = typeof(Resource1), ErrorMessageResourceName = "Department_Name_Required")]
-        public string Name { get; set; }
-        [Range(0, 100, ErrorMessage = "請輸入合理的預算範圍 ({1} ~ {2})")]
-        public decimal Budget { get; set; }
-
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public System.DateTime StartDate { get; set; }
-
-        [UIHint("InstructorID")]
-        public Nullable<int> InstructorID { get; set; }
     }
+    public partial class DepartmentMetaData : DepartmentEditMetaData
+    {
+        public int DepartmentID { get; set; }
+    }
+
 }
