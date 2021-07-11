a6b841b85ab33b8e86e5a7055ff9282d85caf910
Will <doggy.huang@gmail.com>
2021-07-11 17:45:05 +0800

    示範模型驗證的實作方法 IValidatableObject

--- MVCCourse210710/Models/Department.Partial.cs
+++ MVCCourse210710/Models/Department.Partial.cs
@@ -5,16 +5,33 @@ namespace MVCCourse210710.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(DepartmentMetaData))]
-    public partial class Department
+    public partial class Department : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (HasDirtyWord(Name))
+            {
+                yield return new ValidationResult("名稱不能包含髒話", new string[] { "Name" });
+            }
+        }
+
+        private bool HasDirtyWord(string name)
+        {
+            if (name.Contains("Fxxx"))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 
     public partial class DepartmentCreateMetaData
     {
         [Required(ErrorMessageResourceType = typeof(Resource1), ErrorMessageResourceName = "Department_Name_Required")]
-        [是否為身份證字號]
+        //[是否為身份證字號]
         public string Name { get; set; }
         //[Range(0, 100, ErrorMessage = "請輸入合理的預算範圍 ({1} ~ {2})")]
         //[BudgetRange(minValue: 10, maxValue: 1000)]
