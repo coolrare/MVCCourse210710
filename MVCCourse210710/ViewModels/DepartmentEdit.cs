dffcb31a822d5b3d5d356fe2ecce34fc08c49c0f
Will <doggy.huang@gmail.com>
2021-07-11 12:25:28 +0800

    修正 StartDate 在編輯模式的時候格式錯誤的問題

--- MVCCourse210710/ViewModels/DepartmentEdit.cs
+++ MVCCourse210710/ViewModels/DepartmentEdit.cs
@@ -13,6 +13,8 @@ namespace MVCCourse210710.ViewModels
         public string Name { get; set; }
         [Range(0, 99999, ErrorMessage = "請輸入合理的預算範圍 ({1} ~ {2})")]
         public decimal Budget { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public System.DateTime StartDate { get; set; }
 
         public Nullable<int> InstructorID { get; set; }
