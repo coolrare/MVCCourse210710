504235b37fa0ca47d6b3f9c4646c64673e7e2547
Will <doggy.huang@gmail.com>
2021-07-17 12:18:21 +0800

    惡搞路由的示範

--- MVCCourse210710/App_Start/RouteConfig.cs
+++ MVCCourse210710/App_Start/RouteConfig.cs
@@ -14,8 +14,13 @@ namespace MVCCourse210710
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}.jsp/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
