1d24adc55d651354d4802b6c8146d1b333f118b8
Will <doggy.huang@gmail.com>
2021-07-18 15:39:20 +0800

    隨堂測驗：使用 Razor 精準排版內容

---
+++ MVCCourse210710/Controllers/VTController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCourse210710.Controllers
+{
+    public class VTController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
