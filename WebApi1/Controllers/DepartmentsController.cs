f02e2034f329b0a8fc153e073cd13f21fdf23d0b
Will 保哥 <doggy.huang@gmail.com>
2021-07-24 18:36:31 +0800

    示範 Web API 開發流程

---
+++ WebApi1/Controllers/DepartmentsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi1.Models;
+
+namespace WebApi1.Controllers
+{
+    public class DepartmentsController : ApiController
+    {
+        private ContosoUniversityEntities db = new ContosoUniversityEntities();
+
+        public DepartmentsController()
+        {
+            db.Configuration.LazyLoadingEnabled = false;
+        }
+
+        // GET: api/Departments
+        public IQueryable<Department> GetDepartment()
+        {
+            return db.Department;
+        }
+
+        // GET: api/Departments/5
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult GetDepartment(int id)
+        {
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        // PUT: api/Departments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDepartment(int id, Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != department.DepartmentID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Departments
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult PostDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Department.Add(department);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = department.DepartmentID }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult DeleteDepartment(int id)
+        {
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            db.Department.Remove(department);
+            db.SaveChanges();
+
+            return Ok(department);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return db.Department.Count(e => e.DepartmentID == id) > 0;
+        }
+    }
+}
